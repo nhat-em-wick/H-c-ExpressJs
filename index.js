4544d4ad42ce591227736b9790e21038da1d3407
NhatNguyen <mnhat233@gmail.com>
2020-09-06 21:09:18 +0700

    use router

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
-// lowdb setup
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-
-const adapter = new FileSync('db.json');
-const db = low(adapter);
-
-db.defaults({ users: [] })
-  .write();
+var userRoute = require('./routes/users.route');
 //
-var shortid = require("shortid");
+
 var port = 3500;
 
-
+var app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
@@ -28,35 +19,7 @@ app.get('/', function(req, res){
     res.render('index');
 });
 
-app.get('/users',function(req, res){
-    res.render('users/', {
-      users : db.get('users').value()
-    });
-});
-
-app.get('/users/search', function(req, res){
-    var q = req.query.q;
-    var matchedUsers = db.get('users').value().filter(function(users){
-        return users.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    });
-    res.render('users/',{ users : matchedUsers});
-})
-
-app.get('/users/create', function(req, res){
-    res.render('users/create');
-});
-
-app.get('/users/:id', function(req, res){
-    var id = req.params.id;
-    var user = db.get('users').find({id:id}).value();
-    res.render('users/view',{user:user});
-});
-
-app.post('/users/create', function(req, res){
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users');
-});
+app.use('/users', userRoute);
 
 app.listen(port, function(){
     console.log('server listening on port ' + port);
